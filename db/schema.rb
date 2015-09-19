0596dd443c455b86f901c9edcc4d5254a4532b18
吉田智哉 <yoshidatomoya@tom.local>
2015-09-19 16:43:18 +0900

    devise

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150919051334) do
+ActiveRecord::Schema.define(version: 20150919061733) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string   "title"
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(version: 20150919051334) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
