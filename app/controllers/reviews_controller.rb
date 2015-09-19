4dbd2619912ed1310680e375da210ece5d0f76d9
吉田智哉 <yoshidatomoya@tom.local>
2015-09-19 15:12:40 +0900

    reviewのform作成

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -3,4 +3,24 @@ class ReviewsController < ApplicationController
     @restaurant = Restaurant.find(params[:restaurant_id])
     @review = Review.new
   end
+
+  def create
+    @review= Review.new(review_params)
+    @review.user_id = current_user.id
+    @review.restaurant_id = params[:restaurant_id]
+    if @review.save
+      redirect_to @review.restaurant
+    else
+      render :new
+    end
+  end
+
+ private
+ def review_params
+  params.require(:review).permit(
+    :body,
+    :rate)
 end
+
+end
+
